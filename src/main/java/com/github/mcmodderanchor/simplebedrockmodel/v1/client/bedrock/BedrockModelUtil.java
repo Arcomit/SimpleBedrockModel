8c60d40f307b0d71b7ca2011d380abb56d71ae65
tartaric_acid <baka943@qq.com>
2025-07-12 13:03:53 +0800

    修改包名 - 更改为 com.github.mcmodderanchor - 版本号改成 1.4.0 - 删除两个没有清理的类 - 修正部分命名和作者

---
+++ src/main/java/com/github/mcmodderanchor/simplebedrockmodel/v1/client/bedrock/BedrockModelUtil.java
@@ -0,0 +1,21 @@
+package com.github.mcmodderanchor.simplebedrockmodel.v1.client.bedrock;
+
+import com.github.mcmodderanchor.simplebedrockmodel.v1.client.bedrock.pojo.FaceItem;
+import com.github.mcmodderanchor.simplebedrockmodel.v1.client.bedrock.pojo.FaceUVsItem;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+@OnlyIn(Dist.CLIENT)
+public final class BedrockModelUtil {
+    public static FaceUVsItem singleSouthFace() {
+        return new FaceUVsItem(emptyFace(), emptyFace(), emptyFace(), single16xFace(), emptyFace(), emptyFace());
+    }
+
+    public static FaceItem single16xFace() {
+        return new FaceItem(new float[]{0, 0}, new float[]{16, 16});
+    }
+
+    public static FaceItem emptyFace() {
+        return new FaceItem(new float[]{0, 0}, new float[]{0, 0});
+    }
+}
---
+++ src/main/java/com/github/mcmodderanchor/simplebedrockmodel/v1/client/bedrock/model/BedrockBone.java
@@ -0,0 +1,112 @@
+package com.github.mcmodderanchor.simplebedrockmodel.v1.client.bedrock.model;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import it.unimi.dsi.fastutil.objects.ObjectList;
+import net.minecraft.client.renderer.LightTexture;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import org.joml.Matrix3f;
+import org.joml.Vector3f;
+
+import java.util.Random;
+
+@OnlyIn(Dist.CLIENT)
+public class BedrockBone {
+    private static final Vector3f[] NORMALS = new Vector3f[6];
+    private static final int MAX_LIGHT_TEXTURE = LightTexture.pack(15, 15);
+
+    public final ObjectList<BedrockCube> cubes = new ObjectArrayList<>();
+    private final ObjectList<BedrockBone> children = new ObjectArrayList<>();
+    public BedrockBone parent;
+    public int index = -1;
+    public float x;
+    public float y;
+    public float z;
+    public float xRot;
+    public float yRot;
+    public float zRot;
+    public float xScale = 1;
+    public float yScale = 1;
+    public float zScale = 1;
+    public boolean visible = true;
+    public boolean illuminated = false;
+    public boolean mirror;
+
+    static {
+        for (int i = 0; i < NORMALS.length; i++) {
+            NORMALS[i] = new Vector3f();
+        }
+    }
+
+    public void render(PoseStack poseStack, VertexConsumer consumer, int lightmap, int overlay) {
+        this.render(poseStack, consumer, lightmap, overlay, 1.0F, 1.0F, 1.0F, 1.0F);
+    }
+
+    public void render(PoseStack poseStack, VertexConsumer consumer, int lightmap, int overlay, float red, float green, float blue, float alpha) {
+        int cubePackedLight = illuminated ? MAX_LIGHT_TEXTURE : lightmap;
+        if (this.visible) {
+            // 缩放过小时，直接退出渲染
+            boolean xNearZero = -1E-5F < xScale && xScale < 1E-5F;
+            boolean yNearZero = -1E-5F < yScale && yScale < 1E-5F;
+            boolean zNearZero = -1E-5F < zScale && zScale < 1E-5F;
+            if ((xNearZero && yNearZero) || (xNearZero && zNearZero) || (yNearZero && zNearZero)) {
+                return;
+            }
+
+            if (!this.cubes.isEmpty() || !this.children.isEmpty()) {
+                poseStack.pushPose();
+                this.translateAndRotateAndScale(poseStack);
+                this.compile(poseStack.last(), consumer, cubePackedLight, overlay, red, green, blue, alpha);
+
+                for (BedrockBone part : this.children) {
+                    part.render(poseStack, consumer, cubePackedLight, overlay, red, green, blue, alpha);
+                }
+
+                poseStack.popPose();
+            }
+        }
+    }
+
+    public void translateAndRotateAndScale(PoseStack poseStack) {
+        poseStack.translate(this.x / 16.0F, this.y / 16.0F, this.z / 16.0F);
+        if (this.xRot != 0.0F || this.yRot != 0.0F || this.zRot != 0.0F) {
+            poseStack.last().pose().rotateZYX(this.zRot, this.yRot, this.xRot);
+            poseStack.last().normal().rotateZYX(this.zRot, this.yRot, this.xRot);
+        }
+        if (this.xScale != 0.0F || this.yScale != 0.0F || this.zScale != 0.0F) {
+            poseStack.last().pose().scale(this.xScale, this.yScale, this.zScale);
+            poseStack.last().normal().scale(this.xScale, this.yScale, this.zScale);
+        }
+    }
+
+    private void compile(PoseStack.Pose pose, VertexConsumer consumer, int lightmap, int overlay, float red, float green, float blue, float alpha) {
+        Matrix3f normal = pose.normal();
+        NORMALS[0].set(-normal.m10, -normal.m11, -normal.m12);
+        NORMALS[1].set(normal.m10, normal.m11, normal.m12);
+        NORMALS[2].set(-normal.m20, -normal.m21, -normal.m22);
+        NORMALS[3].set(normal.m20, normal.m21, normal.m22);
+        NORMALS[4].set(-normal.m00, -normal.m01, -normal.m02);
+        NORMALS[5].set(normal.m00, normal.m01, normal.m02);
+        for (BedrockCube bedrockCube : this.cubes) {
+            bedrockCube.compile(pose, NORMALS, consumer, lightmap, overlay, red, green, blue, alpha);
+        }
+    }
+
+    public BedrockCube getRandomCube(Random random) {
+        return this.cubes.get(random.nextInt(this.cubes.size()));
+    }
+
+    public boolean isEmpty() {
+        return this.cubes.isEmpty();
+    }
+
+    public void addChild(BedrockBone model) {
+        this.children.add(model);
+    }
+
+    public ObjectList<BedrockBone> getChildren() {
+        return children;
+    }
+}
